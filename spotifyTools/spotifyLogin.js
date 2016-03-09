fd74828ffe1606c10c030419de4f285e71b5580d
Clayton Warren <claytonwarren@me.com>
2016-03-09 13:28:13 -0500

    new branch of modularization with smaller changes for each commit that can be tested

---
+++ spotifyTools/spotifyLogin.js
@@ -0,0 +1,13 @@
+module.exports.spotifyLogin = function (req, res) {
+  var state = tools.generateRandomString(16);
+  res.cookie(stateKey, state);
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-read-birthdate streaming playlist-modify-private playlist-modify-public playlist-read-private';
+  res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
+    response_type: 'code',
+    client_id: client_id,
+    scope: scope,
+    redirect_uri: redirect_uri,
+    state: state
+  }));
+}
