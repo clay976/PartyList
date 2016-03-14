b0302d96bb7966fd5ac64b95c6a5cac54ec17c8d
Clayton Warren <claytonwarren@me.com>
2016-03-14 02:26:53 -0400

    wrong variable name for the message sent handler

--- messageTools/responses.js
+++ messageTools/responses.js
@@ -8,43 +8,43 @@ var query = require ('../databasetools/querydb')
 function notGuest (toNum){
 	var responseBody = ('sorry, you are not a guest of a party, you can send back a host code for a party. We have also send the host a text with your number in case they want to add it themselves')
 	messageObject = messageTool.message (toNum, responseBody)
-	twilio.sendMessage(messageObject, sentHandler)
+	twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function emptyConfirmation (toNum){
   var responseBody = ('\n\nWe don\'t have a request for you to confirm or decline. \n\nIf you song is just "yes", or "no", add an artist name to search')
   messageObject = messageTool.message (toNum, responseBody)
-  twilio.sendMessage(messageObject, sentHandler)
+  twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function requestedAlready (toNum, reqsLeft, trackRequests){
   var responseBody = ('\n\nThis track has already been requested, Your request will bump it up in the queue!\n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has ' +trackRequests+ ' requests!')
   messageObject = messageTool.message (toNum, responseBody)
-  twilio.sendMessage(messageObject, sentHandler)
+  twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function newRequest (toNum, reqsLeft){
   var responseBody = ('\n\nThis track is new!! \n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has 1 request!')
   messageObject = messageTool.message (toNum, responseBody)
-  twilio.sendMessage(messageObject, sentHandler)
+  twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function declineRequest (toNum){
 	var responseBody = ('\n\nSorry about the wrong song, try modifying your search! Remember to not use any special characters.')
 	messageObject = messageTool.message (sender, responseBody)
-	twilio.sendMessage(messageObject, sentHandler)
+	twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function songNotFound (toNum){
   var responseBody = ('sorry, that song could be found, use as many key words as possible, make sure to not use any special characters either!')
   messageObject = messageTool.message (sender, responseBody)
-  twilio.sendMessage(messageObject, sentHandler)
+  twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function advertisment (toNum){
 	var responseBody = ('\n\nYou are recieving an advertisment because you have made 5 successful request')
 	messageObject = messageTool.message (sender, responseBody)
-	twilio.sendMessage(messageObject, sentHandler)
+	twilio.sendMessage(messageObject, messageTool.sentHandler)
 }
 
 function askConfirmation(db, toNum, trackAdd){
@@ -61,7 +61,7 @@ function askConfirmation(db, toNum, trackAdd){
       responseBody = ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n This request is new!! \n\nSend back "Yes" to confirm, "No" to discard this request!')
     }
     messageObject = messageTool.message (toNum, responseBody)
-    twilio.sendMessage(messageObject, sentHandler)
+    twilio.sendMessage(messageObject, messageTool.sentHandler)
   })
 }
 
