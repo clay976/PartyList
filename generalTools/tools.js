9c94a1ca1c09a816fbffccd72a2fe9c129c20169
Clayton Warren <claytonwarren@me.com>
2016-03-14 02:09:51 -0400

    error with semicolons not in the for loop of tools after replacment

--- generalTools/tools.js
+++ generalTools/tools.js
@@ -1,7 +1,7 @@
 module.exports.generateRandomString = function(length) {
   var text = ''
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  for (var i = 0 i < length i++) {
+  for (var i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
   return text
