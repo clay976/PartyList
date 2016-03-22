ffdaa70793322856517f78c7ecd4215e5401f1a2
Clayton Warren <claytonwarren@me.com>
2016-03-21 22:06:34 -0400

    I think this is how the response should be structures. was not making the resp variable .message correctly

--- messageTools/responses.js
+++ messageTools/responses.js
@@ -12,7 +12,7 @@ function notGuest (res){
 
   resp.message('\n\nsorry, you are not a guest of a party, you can send back a host code for a party. We have also send the host a text with your number in case they want to add it themselves');
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function emptyConfirmation (res){
@@ -20,7 +20,7 @@ function emptyConfirmation (res){
 
   resp.message('\n\nWe don\'t have a request for you to confirm or decline. \n\nIf you song is just "yes", or "no", add an artist name to search')
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function requestedAlready (res, reqsLeft, trackRequests){
@@ -28,31 +28,31 @@ function requestedAlready (res, reqsLeft, trackRequests){
 
   var responseBody = ('\n\nThis track has already been requested, Your request will bump it up in the queue!\n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has ' +(trackRequests + 1)+ ' requests!')
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function newRequest (res, reqsLeft){
   var resp = new twilio.TwimlResponse();
 
-  resp.message = ('\n\nThis track is new!! \n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has 1 request!')
+  resp.message ('\n\nThis track is new!! \n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has 1 request!')
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function declineRequest (res){
   var resp = new twilio.TwimlResponse();
 
-	resp.message = ('\n\nSorry about the wrong song, try modifying your search! Remember to not use any special characters.')
+	resp.message ('\n\nSorry about the wrong song, try modifying your search! Remember to not use any special characters.')
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function songNotFound (res){
   var resp = new twilio.TwimlResponse();
 
-  resp.message = ('\n\nsorry, that song could be found, use as many key words as possible, make sure to not use any special characters either!')
+  resp.message ('\n\nsorry, that song could be found, use as many key words as possible, make sure to not use any special characters either!')
   res.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.end(resp.toString());
+  res.send(resp.toString());
 }
 
 function advertisment (res){
@@ -73,12 +73,12 @@ function askConfirmation(res, db, trackAdd){
   query.search ('tracks', trackObjID, db, function (trackDocFound){
     if (trackDocFound){
       var currentSongRequests = trackDocFound.numRequests
-      resp.message = ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n Current number of requests: ' +currentSongRequests+ '\n\nSend back "Yes" to confirm, "No" to discard this request!')
+      resp.message ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n Current number of requests: ' +currentSongRequests+ '\n\nSend back "Yes" to confirm, "No" to discard this request!')
     }else{
-      resp.message = ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n This request will be new!! \n\nSend back "Yes" to confirm, "No" to discard this request!')
+      resp.message ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n This request will be new!! \n\nSend back "Yes" to confirm, "No" to discard this request!')
     }
     console.log (resp.toString())
-    res.end(resp.toString());
+    res.send(resp.toString());
   })
 }
 
