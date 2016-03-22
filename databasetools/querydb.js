50024fcbbd6b1fe3cb95838adb9934b283abf559
Clayton Warren <claytonwarren@me.com>
2016-03-21 21:21:13 -0400

    logging more information for debugging

--- databasetools/querydb.js
+++ databasetools/querydb.js
@@ -15,7 +15,12 @@ module.exports.findGuest = function(phoneNum){
     { "phone" : phoneNum }) 
 } 
 
-module.exports.search = function (collect, docu, db, callback){ 
+module.exports.search = function (collect, docu, db, callback){
+  console.log ('collection', collect)
+  console.log ('db: ', db)
+  console.log (docu)
+
+
   var cursor =db.collection(collect).find(docu)
   cursor.toArray(function (err, doc) {
     assert.equal(err, null)
