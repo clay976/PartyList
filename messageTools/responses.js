3b123fb8450cc52cfdcb815a78e24993ec95b119
Clayton Warren <claytonwarren@me.com>
2016-03-22 01:46:20 -0400

    changed back to send instead of end

--- messageTools/responses.js
+++ messageTools/responses.js
@@ -77,7 +77,7 @@ function askConfirmation(res, db, trackAdd){
     }else{
       resp.message ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n This request will be new!! \n\nSend back "Yes" to confirm, "No" to discard this request!')
     }
-    res.send(resp.toString());
+    res.Message(resp.toString());
   })
 }
 
