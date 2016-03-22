eef5caf83a23349ce4cecceafc629d2154c86c42
Clayton Warren <claytonwarren@me.com>
2016-03-21 22:03:03 -0400

    changed some things about the response to twilio

--- messageTools/responses.js
+++ messageTools/responses.js
@@ -11,48 +11,48 @@ function notGuest (res){
   var resp = new twilio.TwimlResponse();
 
   resp.message('\n\nsorry, you are not a guest of a party, you can send back a host code for a party. We have also send the host a text with your number in case they want to add it themselves');
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function emptyConfirmation (res){
   var resp = new twilio.TwimlResponse();
 
   resp.message('\n\nWe don\'t have a request for you to confirm or decline. \n\nIf you song is just "yes", or "no", add an artist name to search')
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function requestedAlready (res, reqsLeft, trackRequests){
   var resp = new twilio.TwimlResponse();
 
   var responseBody = ('\n\nThis track has already been requested, Your request will bump it up in the queue!\n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has ' +(trackRequests + 1)+ ' requests!')
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function newRequest (res, reqsLeft){
   var resp = new twilio.TwimlResponse();
 
   resp.message = ('\n\nThis track is new!! \n\n Requests before next ad: ' +reqsLeft+ '\n\n This song now has 1 request!')
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function declineRequest (res){
   var resp = new twilio.TwimlResponse();
 
 	resp.message = ('\n\nSorry about the wrong song, try modifying your search! Remember to not use any special characters.')
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function songNotFound (res){
   var resp = new twilio.TwimlResponse();
 
   resp.message = ('\n\nsorry, that song could be found, use as many key words as possible, make sure to not use any special characters either!')
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
-  res.send(resp.toString());
+  res.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.end(resp.toString());
 }
 
 function advertisment (res){
@@ -68,7 +68,7 @@ function askConfirmation(res, db, trackAdd){
 	var trackID =trackAdd.tracks.items[0].id
 	var trackTitle = trackAdd.tracks.items[0].name
 	var trackArtist = trackAdd.tracks.items[0].artists[0].name
-  resp.writeHead =(200, {'Content-Type': 'text/xml'});
+  res.writeHead = (200, {'Content-Type': 'text/xml'});
 
   query.search ('tracks', trackObjID, db, function (trackDocFound){
     if (trackDocFound){
@@ -78,7 +78,7 @@ function askConfirmation(res, db, trackAdd){
       resp.message = ('track found: ' +trackTitle+ ' by ' +trackArtist+ '\n\n This request will be new!! \n\nSend back "Yes" to confirm, "No" to discard this request!')
     }
     console.log (resp.toString())
-    res.send(resp.toString());
+    res.end(resp.toString());
   })
 }
 
