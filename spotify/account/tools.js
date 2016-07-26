a0f599cbc54903958697e38ac53c494b50129159
Clayton Warren <claytonwarren@me.com>
2016-07-26 00:27:27 -0400

    error fix from last commit

--- spotify/account/tools.js
+++ spotify/account/tools.js
@@ -37,6 +37,7 @@ function login (req, res) {
 // preps them in to an "options" object to
 // make another call for host info
 function homepage (req, res, db) {
+  console.log (req.query.code)
   spotifyApi.authorizationCodeGrant(req.query.code)
   .then(function(data) {
     console.log('The token expires in ' + data.body['expires_in']);
