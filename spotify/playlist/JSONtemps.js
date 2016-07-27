7a8888d8fa8cfbafc89b186305a999010bf7b7b8
Clayton Warren <claytonwarren@me.com>
2016-07-27 17:58:36 -0400

    error fixing

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,12 +1,12 @@
 function userPlaylists (data){
   var playlists = data.body.items
+  console.log ((playlists).length())
+  
   for (var index = 0; index < (playlists).length(); index ++){
     console.log (playlists [index].id)
     console.log (playlists [index].name)
   }
-  return {
-    "playlists" :  data.body.items
-  }
+  return (data.body.items)
 }
 
 module.exports = {
