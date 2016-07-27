bf8c34a85bc1ec16294e36af6ff68460d1ddfa9b
Clayton Warren <claytonwarren@me.com>
2016-07-27 12:35:20 -0400

    error fix on last commit

--- spotify/account/tools.js
+++ spotify/account/tools.js
@@ -34,9 +34,7 @@ function homepage (req, res, db) {
     var access_token = data.body['access_token']
     var refresh_token = data.body['refresh_token']
     var hostInfo = (spotifyApi.getMe()).then (function (hostInfo){
-      var updateInfo = upsertTemplate.Host (hostInfo, access_token, refresh_token).then (function (updateInfo){ 
-        model.Host.findOneAndUpdate({hostID: hostInfo.body.id}, updateInfo, {upsert:true})
-      })
+      model.Host.findOneAndUpdate({hostID: hostInfo.body.id}, upsertTemplate.Host (hostInfo, access_token, refresh_token), {upsert:true})
     })
   })
   .catch(function(err) {
