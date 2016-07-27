0783e2646681ce87054938014ed108e2ac94dc01
Clayton Warren <claytonwarren@me.com>
2016-07-27 10:48:53 -0400

    error fixing from last commit

--- app.js
+++ app.js
@@ -25,7 +25,8 @@ var guestTools = require ('./database/guestTools')
 
 //mongo database variables
 var MongoClient = require('mongodb').MongoClient
-var mongoose = require('mongoose');
+var Promise = require('bluebird');
+var mongoose = Promise.promisifyAll(require("mongoose"))
 var mongoUrl = 'mongodb://localhost:27017/party'
 
 //connect to the database, this happens when api starts, and the conection doesn't close until the API shuts down/crashes
