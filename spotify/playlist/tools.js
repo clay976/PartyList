a6c3fcbdf3483b5db70831e922b5323bc410e65c
Clayton Warren <claytonwarren@me.com>
2016-07-27 18:38:02 -0400

    error fixing

--- spotify/playlist/tools.js
+++ spotify/playlist/tools.js
@@ -74,7 +74,7 @@ function findAllPlaylists (req, res, db){
     spotifyApi.setAccessToken(hostInfo.access_token)
     spotifyApi.getUserPlaylists(hostInfo.hostID)
     .then (function(data){
-      var length = data.total
+      var length = data
       console.log (length)
 
       var playlists = '{ playlists: { name: ' +data[0].name+ ', id : ' +data[0].id+ '}'
