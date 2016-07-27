c57354c80d07d91d98329438217c46ef351e32a0
Clayton Warren <claytonwarren@me.com>
2016-07-27 19:44:45 -0400

    changed the res.send to res.json as that is what it should be

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,9 +1,9 @@
 function userPlaylists (playlists, length){
-  var playlistJSON = '{ "playlists": [{"name": "' +playlists[0].name+ '", "id" : "' +playlists[0].id+ '"}'
+  var playlistJSON = "{ 'playlists': [{'name': '" +playlists[0].name+ ", 'id' : '" +playlists[0].id+ "}"
   for (var index = 1; index < length; index ++){
-    playlistJSON = playlistJSON + ', {"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ '"}'
+    playlistJSON = playlistJSON + ", {'name': '" +playlists[index].name+ ", 'id' : '" +playlists[index].id+ "}"
   }
-  return (playlistJSON + ']}')
+  return (playlistJSON + "]}")
 }
 
 module.exports = {
