4a4bf3f1c686f4cf892425b4d7bf815a2665d7bb
Clayton Warren <claytonwarren@me.com>
2016-07-27 19:10:02 -0400

    JSON object to be sent back to the client is being created properly. will be moving on to the next endpoint after any additional error fixing

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,7 +1,7 @@
 function userPlaylists (playlists, length){
-  var playlistJSON = '{ playlists: { name: ' +playlists[0].name+ ', id : ' +playlists[0].id+ '}'
+  var playlistJSON = '{ "playlists": { "name": "' +playlists[0].name+ '", "id" : "' +playlists[0].id+ '"}'
   for (var index = 1; index < length; index ++){
-    playlistJSON = playlistJSON + ', { name: ' +playlists[index].name+ ', id : ' +playlists[index].id+ '}'
+    playlistJSON = playlistJSON + ', { "name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ '"}'
   }
   console.log (playlistJSON)
   return playlistJSON
