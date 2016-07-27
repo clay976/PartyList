80d20329426bf85727404fab209b5dd506738f3f
Clayton Warren <claytonwarren@me.com>
2016-07-27 17:56:21 -0400

    for loop was defined incorrectly

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,6 +1,6 @@
 function userPlaylists (data){
   var playlists = data.body.items
-  for (var index = 0, index < (playlists).length(), index ++){
+  for (var index = 0; index < (playlists).length(); index ++){
     console.log (playlists [index].id)
     console.log (playlists [index].name)
   }
