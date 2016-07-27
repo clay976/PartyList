b78f6f5e4fe781f3960bbf9598a44b24832b2aa4
Clayton Warren <claytonwarren@me.com>
2016-07-27 18:24:03 -0400

    error fixing

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,8 +1,8 @@
 function userPlaylists (data){
   var length = data.total
-  var playlists = '{ playlists: { name: ' +data[0].name+ ', id : ' data[0].id+ '}'
+  var playlists = '{ playlists: { name: ' +data[0].name+ ', id : ' +data[0].id+ '}'
   for (var index = 1; index < length; index ++){
-    playlists + ', { name: ' +data[index].name+ ', id : ' data[index].id+ '}'
+    playlists + ', { name: ' +data[index].name+ ', id : ' +data[index].id+ '}'
   }
   return (playlists + '}')
 }
