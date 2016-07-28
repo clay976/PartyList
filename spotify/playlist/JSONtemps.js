4a7cef72394f60eecdb02ba1edcc642e8cff555f
Clayton Warren <claytonwarren@me.com>
2016-07-27 20:29:18 -0400

    error fix on last commit

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -1,11 +1,9 @@
 function userPlaylists (host, playlists, length){
 	var playlistJSON = '{ "playlists": ['
-	if (playlists[0].owner.id === host) {
-  	playlistJSON = playlistJSON+ '{"name": "' +playlists[0].name+ '", "id" : "' +playlists[0].id+ ', "owner": "'+playlists[0].owner.id+'"}'
-  }
   for (var index = 1; index < length; index ++){
   	if (playlists[index].owner.id === host){
-    	playlistJSON = playlistJSON + ', {"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ ', "owner": "'+playlists[index].owner.id+'"}'
+    	playlistJSON = playlistJSON + '{"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ ', "owner": "'+playlists[index].owner.id+'"}'
+    	if (index < (length-1)) playlistJSON = playlistJSON + ','
  		}
   }
   return (JSON.parse (playlistJSON + ']}'))
