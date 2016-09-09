5d65b6b2615951d018b32256014642fff22ab3f4
Clayton Warren <claytonwarren@me.com>
2016-09-09 16:10:50 -0400

    still fixing

--- database/guestTools.js
+++ database/guestTools.js
@@ -43,7 +43,7 @@ function validateGuest (body){
         reject ('could not find this document in our database, this may be a problem on our end, sorry!')
       }
     })
-    .catch (console.log ('validating guest'))
+    .catch (console.log ('validating guest failed'))
   })
 }
 
