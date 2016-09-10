d37a445eb4fa04eaf34b65dc09ed93a8270008b8
Clayton Warren <claytonwarren@me.com>
2016-09-09 20:13:35 -0400

    still fixing

--- twilio/incoming.js
+++ twilio/incoming.js
@@ -40,7 +40,7 @@ function businessLogic (req, res, db){
       console.log ('searching for tracks with name: '+ messageBody)
       spotifyApi.searchTracks (messageBody, { limit : 1 })
       .then (function (tracksFound){
-        console.log ('tracks: 'tracksFound)
+        console.log ('tracks: ' +tracksFound)
         console.log ('trackID: ' +trackAdd.tracks.items[0])
         model.Track.findOneandUpdate({'trackID': trackAdd.tracks.items[0].id}, upsertTemplate (trackAdd.tracks.items[0].id), {upsert:true}).exec()
         .then (function (trackFound){
