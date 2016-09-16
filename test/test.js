1100852c4a78da87fdc2b45e54a8a83d977c23fb
Clayton Warren <claytonwarren@me.com>
2016-09-16 18:56:44 -0400

    first commit with possible working test cases. Will have to change things like with the server adress for proper intergrating testing

--- test/test.js
+++ test/test.js
@@ -13,29 +13,8 @@ describe('GET /guests/add', function(){
         throw err;
       }
       // this is should.js syntax, very clear
-      res.should.have.status(400);
+      res.status.should.equal(200);
       done();
     });
   });
-})
-
-describe('test building JSON object to send message with twilio', function(){
-  it('should return a properly built message with body as "hello"', function(){
-  	var tool = require ('../messageTools/message')
-  	var number = '+01234567890'
-  	var body = 'hello'
-  	var mess = tool.message (number, body)
-
-  	console.log (mess)
-    assert.equal (mess.to, number)
-    assert.equal (mess.body, body)
-  })
-
-  it('should return the message object as an error string instead of a JSON object', function(){
-  	var tool = require ('../messageTools/message')
-  	var mess = tool.message ()
-
-  	console.log (mess)
-    assert.equal (mess, 'error, parameters incorrect for message object')
-  })
 })
