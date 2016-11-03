d4d10c1f0416e256b1b1cfbacb98d6b02024580a
Clayton Warren <claytonwarren@me.com>
2016-11-03 11:17:15 -0400

    this is almost working for multiple users. Testing hpw the app behavious when a guest is moveed around parties

--- spotify/account/tools.js
+++ spotify/account/tools.js
@@ -3,6 +3,7 @@
 
 //my modules
 var databaseHostTools = require ('../../database/hostTools')
+var playlistTool = require ('../playlist/tools')
 
 var SpotifyWebApi = require('spotify-web-api-node');
 var credentials = {
@@ -21,14 +22,14 @@ var querystring = require('querystring')
 // preps them in to an "options" object to
 // make another call for host info
 function homepage (req, res) {
-  var homePage
   spotifyApi.authorizationCodeGrant(req.query.code)
   .then (function (data){
     return setTokensAndGetHostInfo(data)
   })
   .then (function (hostInfo){
     console.log (hostInfo)
-    homePage = '/#' +querystring.stringify({'access_token': hostInfo.access_token,'refresh_token':hostInfo.refresh_token,'hostID':hostInfo.spotifyReturn.body.id})
+    var homePage = '/#' +querystring.stringify({'access_token': hostInfo.access_token,'refresh_token':hostInfo.refresh_token,'hostID':hostInfo.spotifyReturn.body.id})
+    playlistTool.setLatestPlaylist ({'req':{'body':{'hostID':hostInfo.spotifyReturn.body.id}}})
     return model.Host.findOneAndUpdate({'hostID': hostInfo.spotifyReturn.body.id}, upsertTemplate.Host (hostInfo.spotifyReturn.body.id, hostInfo.access_token, hostInfo.refresh_token, homePage), {upsert:true}).exec()
   })
   .then (function (host){
