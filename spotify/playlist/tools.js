c685a60904b454b59379a935e88591e6a7f22115
Clayton Warren <claytonwarren@me.com>
2017-06-05 15:08:40 -0400

    fixing settign specific spalist with the ID

--- spotify/playlist/tools.js
+++ spotify/playlist/tools.js
@@ -70,6 +70,8 @@ function setSpecificPlaylist (req, res, db){
     return validatePlaylistOwnership (validatedInput)
   })
   .then (function (validRequest){
+    console.log (validRequest.HostID)
+    console.log (validRequest.playlistID)
     return (model.Host.findOneAndUpdate({ 'hostID' : validRequest.HostID }, { $set: {'playlistID' : validRequest.playlistID}}).exec())
   })
   .then (function (updated){
