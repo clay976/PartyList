1c3e007227bb54ff13f0b0c7623d575f8631be60
Clayton Warren <claytonwarren@me.com>
2017-06-11 21:11:54 -0400

    fixed the response when setting the minimum or maximum year from the front end

--- database/hostTools.js
+++ database/hostTools.js
@@ -59,7 +59,7 @@ function minYear (req, res){
   .then (function (hostInfo){
     model.Host.findOneAndUpdate({ 'hostID' : hostInfo.hostID }, { $set: {'minYear' : req.body.year}}).exec()
   })
-  .then (res.status(200).json ('explicit filter successfully set to ' +req.body.explicit))
+  .then (res.status(200).json ('minimum year of songs set to ' +req.body.year))
   .catch (function(err) {
     res.status(err.status).json('failed to set explicit filter, ' +err)
     //fixed option: filter out genres next: go to sleep with girlfriend (all actively playing paties: katya), requested songs: sleep, songs requested: sleep
@@ -71,7 +71,7 @@ function maxYear (req, res){
   .then (function (hostInfo){
     model.Host.findOneAndUpdate({ 'hostID' : hostInfo.hostID }, { $set: {'maxYear' : req.body.year}}).exec()
   })
-  .then (res.status(200).json ('explicit filter successfully set to ' +req.body.explicit))
+  .then (res.status(200).json ('maximum year of songs set to ' +req.body.year))
   .catch (function(err) {
     res.status(err.status).json('failed to set explicit filter, ' +err)
     //fixed option: filter out genres next: go to sleep with girlfriend (all actively playing paties: katya), requested songs: sleep, songs requested: sleep
@@ -90,6 +90,8 @@ function verifyExplicitFilter (guestObject) {
 
 function verifyYearFilter (guestObject) {
   return new Promise (function (fulfill, reject){
+    console.log (guestObject.host)
+    console.log (guestObject.track)
     fulfill (guestObject)
   })
 }
