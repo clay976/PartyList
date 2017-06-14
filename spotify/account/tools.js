14cf847b2821164d0bf77eaea8f3fce4658949ea
Clayton Warren <claytonwarren@me.com>
2017-06-14 17:17:41 -0400

    fixing errors that relate to last commit

--- spotify/account/tools.js
+++ spotify/account/tools.js
@@ -70,7 +70,7 @@ function setPlaylistOnLogin (hostInfo){
       return playlistTemplate.userPlaylists (hostInfo.host.id, playlists.body.items, playlists.body.total)
     })
     .then (function (playlists){
-      console.log (playlists [0])
+      console.log (playlists [0][0])
       hostInfo.playlist = playlists
       fulfill (hostInfo)
     })
