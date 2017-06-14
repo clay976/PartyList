308c4067fe36d1262bac9985fcb56f6e14ff1ef5
Clayton Warren <claytonwarren@me.com>
2017-06-13 21:51:30 -0400

    trying to fix the fill out of the forms

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -3,8 +3,7 @@ function userPlaylists (host, playlists, length){
   for (var index = 0; index < length; index ++){
   	if (playlists[index].owner.id === host){
   		console.log (playlistJSON)
-    	playlistJSON = playlistJSON + '{"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ '", "owner": "'+playlists[index].owner.id+'"}'
-    	if (index < (length-1)) playlistJSON = playlistJSON + ','
+    	playlistJSON = playlistJSON + '{"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ '", "owner": "'+playlists[index].owner.id+'"},'
  		}
   }
   console.log ('done creating')
