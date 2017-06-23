f4f1207f54b0054f4fadd0026b80276dc93c5399
Clayton Warren <claytonwarren@me.com>
2017-06-23 12:24:01 -0400

    fixing next function

--- spotify/track/tools.js
+++ spotify/track/tools.js
@@ -2,17 +2,18 @@ var hostAcountTools	= require ('../../database/hostTools')
 
 function obtainYearReleased (guestObject){
 	return new Promise (function (fulfill, reject){
-		for (var index = 0; index < 4; index ++){
-			hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[index].albumID)
-			.then (function(album){
-				var date = album.body.release_date.substring(0, 4);
-				guestObject.tracks[index].yearReleased = date
-			})
-			.catch (function (err){
-				reject (err)
-			})
-		}
-		guestObject.tracks[3].yearReleased.then (fulfill (guestObject))
+		guestObject.tracks[0].yearReleased = hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[0].albumID)
+		guestObject.tracks[1].yearReleased = hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[1].albumID)
+		guestObject.tracks[2].yearReleased = hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[2].albumID)
+		hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[3].albumID)
+		.then (function(album){
+			var date = album.body.release_date.substring(0, 4);
+			guestObject.tracks[3].yearReleased = date
+			fulfill (guestObject)
+		})
+		.catch (function (err){
+			reject (err)
+		})
 	})
 }
 
