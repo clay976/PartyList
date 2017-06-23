70a00ddc3ef7aa45a98be106f479a0de3d42d31c
Clayton Warren <claytonwarren@me.com>
2017-06-22 21:41:15 -0400

    fixing next function

--- spotify/track/tools.js
+++ spotify/track/tools.js
@@ -5,9 +5,9 @@ function obtainYearReleased (guestObject){
 		for (var index = 0; index < 4; index ++){
 			hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[index].albumID)
 			.then (function(album){
+				console.log (album.body.release_date)
 				var date = album.body.release_date.substring(0, 4);
 				guestObject.tracks[index].yearReleased = date
-				console.log (guestObject.tracks[index].yearReleased)
 			})
 			.catch (function (err){
 				reject (err)
