df9306aa8257a6605672407e906edb50900415cb
Clayton Warren <claytonwarren@me.com>
2017-06-23 12:18:31 -0400

    fixing next function

--- spotify/track/tools.js
+++ spotify/track/tools.js
@@ -1,17 +1,19 @@
 var hostAcountTools	= require ('../../database/hostTools')
 
 function obtainYearReleased (guestObject){
-	for (var index = 0; index < 4; index ++){
-		hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[index].albumID)
-		.then (function(album){
-			var date = album.body.release_date.substring(0, 4);
-			guestObject.tracks[index].yearReleased = date
-		})
-		.catch (function (err){
-			reject (err)
-		})
-	}
-	return (guestObject)
+	return new Promise (function (fulfill, reject){
+		for (var index = 0; index < 4; index ++){
+			hostAcountTools.spotifyApi.getAlbum (guestObject.tracks[index].albumID)
+			.then (function(album){
+				var date = album.body.release_date.substring(0, 4);
+				guestObject.tracks[index].yearReleased = date
+			})
+			.catch (function (err){
+				reject (err)
+			})
+		}
+		guestObject.tracks[3].yearReleased.then (fulfill (guestObject))
+	})
 }
 
 module.exports = {
