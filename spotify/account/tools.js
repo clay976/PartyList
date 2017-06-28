bc37a7e69ef67f45c30215ae98b6f30623e5bdd0
Clayton Warren <claytonwarren@me.com>
2017-06-27 20:21:56 -0400

    moving back to make sure it works

--- spotify/account/tools.js
+++ spotify/account/tools.js
@@ -31,7 +31,7 @@ function homepage (req, res) {
     return setPlaylistOnLogin (hostInfo)
   })
   .then (function (hostInfo){
-    console.log (hostInfo.playlists)
+    var playlists = hostInfo.playlists
     var homePage = '/loggedIn.html#' +querystring.stringify({'hostID':hostInfo.host.id, 'playlistID': playlists[0].id})
     model.Host.findOneAndUpdate({'hostID': hostInfo.host.id}, JSONtemplate.Host (hostInfo.host.id, hostInfo.access_token, hostInfo.refresh_token, homePage, playlists[0].id, playlists[0].name), {upsert:true}).exec()
     return (homePage)
