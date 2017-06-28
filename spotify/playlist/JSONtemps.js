bf5608451e8fe44e101053cbdd0ce1c9329b45fc
Clayton Warren <claytonwarren@me.com>
2017-06-27 20:17:54 -0400

    moving back to make sure it works

--- spotify/playlist/JSONtemps.js
+++ spotify/playlist/JSONtemps.js
@@ -2,7 +2,7 @@ function userPlaylists (host, playlists, length){
 	if (length > 20){
 		length = 20
 	}
-	var playlistJSON = '{playlists: ['
+	var playlistJSON = '{"playlists": ['
   for (var index = 0; index < length; index ++){
   	if (playlists[index].owner.id === host){
     	playlistJSON = playlistJSON + '{"name": "' +playlists[index].name+ '", "id" : "' +playlists[index].id+ '", "owner": "'+playlists[index].owner.id+'"},'
