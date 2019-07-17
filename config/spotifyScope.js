2f60ec96be51497529acffc6447818515702f678
claytonwarren <clayton.warren@capco.com>
2019-07-17 14:10:42 -0400

    enviroment variables for spotify redirect URI were not set up properly

--- config/spotifyScope.js
+++ config/spotifyScope.js
@@ -7,19 +7,10 @@ dotenv.config();
 
 //Spotify
 var spotifyUserScope  = 'user-read-private user-read-email user-read-birthdate streaming playlist-modify-private playlist-modify-public playlist-read-private'
-var spotifyRedirectURI
-
-if (process.env.NODE_ENV == 'DEV'){
-	spotifyRedirectURI = "http://localhost/login/callback"
-}else if (process.env.NODE_ENV == 'PROD'){
-	spotifyRedirectURI = "http://criwcomputing.com/login/callback"
-}else{
-
-}
 
 module.exports = {
 	response_type     : 'code',
 	client_id         : process.env.SPOTIFY_ID,
 	scope             : spotifyUserScope,
-	redirect_uri      : spotifyRedirectURI
+	redirect_uri      : process.env.SPOTIFY_REDIRECT
 }
