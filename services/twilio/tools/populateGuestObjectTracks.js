2aab65d9a69bb4fdd5a6ba2c92ff9cbdc4102e35
claytonwarren <clayton.warren@capco.com>
2019-07-24 14:33:02 -0400

    logging tracks found, to see why some errors are filtering to client side

--- services/twilio/tools/populateGuestObjectTracks.js
+++ services/twilio/tools/populateGuestObjectTracks.js
@@ -1,8 +1,9 @@
 module.exports = function populateGuestObjectTracks (spotifyTracks){
 	var arr = []
 	var limit = 3
+
 	for (var index = 0; index <= limit; index ++){
-		console.log (spotifyTracks.body.tracks.items[index].name)
+		if (!spotifyTracks.body.tracks.items[index]) spotifyTracks.body.tracks.items[index] = spotifyTracks.body.tracks.items[index - 1]
 		arr[index] = {
 	    'trackID'     : spotifyTracks.body.tracks.items[index].id,
 	    'name'        : spotifyTracks.body.tracks.items[index].name,
