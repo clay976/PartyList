2b25579fb3e9c490cf2aefa8449532cf63c92272
claytonwarren <clayton.warren@capco.com>
2019-07-24 14:54:49 -0400

    logging tracks found, to see why some errors are filtering to client side

--- services/spotify/playlist/setLatestPlaylist.js
+++ services/spotify/playlist/setLatestPlaylist.js
@@ -2,6 +2,8 @@ var validateHost              = require ('database/host/validate')
 var setNewPlaylistHomePage    = require ('database/host/setNewPlaylistHomePage')
 var updatehost                = require ('database/host/update')
 
+var model             = require ('database/models')
+
 var updateLatestPlaylistInfo  = require ('services/spotify/playlist/tools/updateLatestPlaylistInfo')
 var getHostPlaylists          = require ('services/spotify/account/tools/getHostPlaylists')
 
@@ -13,6 +15,16 @@ module.exports = function setLatestPlaylist (host, res){
   .then (getHostPlaylists)
   .then (updateLatestPlaylistInfo)
   .then (setNewPlaylistHomePage)
+  .then (async function (requestObject){
+    let tracks = await model.Track.find ({'hostID' : requestObject.databaseHost.hostID})
+    var promises = tracks.map (async track => {
+      track.addedPlaylist = false
+      track.save()
+      return
+    }
+    let resolution = await Promise.all(promises)
+    return requestObject
+  })
   .then (updatehost)
   .then (function (update){
     res.redirect (host.databaseHost.homePage)
